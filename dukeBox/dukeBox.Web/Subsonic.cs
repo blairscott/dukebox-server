09e0ea7718d4c5aef28783473e9035e72d33c1f3
blairscott <blairiscool@gmail.com>
2014-06-27 05:03:14 -0400

    Added downloadUrl

--- dukeBox/dukeBox.Web/Subsonic.cs
+++ dukeBox/dukeBox.Web/Subsonic.cs
@@ -199,6 +199,7 @@ namespace dukeBox
         public string title;
         public string duration;
         public string url;
+        public string downloadUrl;
         public string imageUrl;
 
         public Song()
@@ -224,7 +225,9 @@ namespace dukeBox
             this.parent = null;
             this.lastAccessed = DateTime.Now.ToString();
             this.url = Subsonic.BuildDirectURL("stream", new Dictionary<string, string> { { "id", this.id } });
-            this.imageUrl = Subsonic.BuildDirectURL("getCoverArt", new Dictionary<string, string> { { "id", this.id } });            
+            this.imageUrl = Subsonic.BuildDirectURL("getCoverArt", new Dictionary<string, string> { { "id", this.id } });
+            this.downloadUrl = Subsonic.BuildDirectURL("download", new Dictionary<string, string> { { "id", this.id } });
+
         }
 
         public Song(string title, string artist, string album, string id, SubsonicItem parent)
